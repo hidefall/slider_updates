cb07d3667851e4eccfdfec176f93ad4e551714ca
hidefall <hidefall@gmail.com>
2017-11-08 16:57:51 +0200

    left border, text-resize, description position updated

---
+++ js/slider.js
@@ -0,0 +1,44 @@
+;(function(jQuery){
+  'use strict';
+
+  $(document).ready(function(){
+    $('.carousel.carousel-slider').carousel({
+      indicators: true,
+      draggable: false,
+    });
+    autoplay()   
+    function autoplay() {
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 4500);
+    }
+    $(".carousel.carousel-slider").click(function(){
+      $(".slide").fadeIn({
+        duration: 9000,
+      });
+    });
+  });
+
+  function idea(){
+    var wh = $(window).height();
+    var bw = parseInt($('.slide_title_border').css('border-left-width'));
+
+    $('.slide_title_border').css({'top':wh/
+      2 - bw, 'left':-(wh/2 - bw)})
+    $('.slide_title_border').width(wh);
+    $('.slide_title_border').width($('.slide_title_border').width() - bw*2)
+    console.log($('.slide_title_border').width() + "---" + $('.slide_title_border').height()) ;
+  }
+  $(document).ready(function(){
+    idea()
+  }); 
+  $(window).resize(function(){
+    idea()
+  });
+
+jQuery("#responsive_headline").fitText(1.4);
+jQuery("#responsive_headline").fitText(0.8);
+
+
+
+
+})(jQuery);
